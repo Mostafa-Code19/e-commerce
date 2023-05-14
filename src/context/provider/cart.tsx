f0f7c32f5e297e481e5e08607bf0b790ad2be133
Mostafa-Code19 <tabrizian.codes@gmail.com>
2023-05-14 09:42:55 +0330

    add empty-cart message in cart storage cart context in localStorage for server reloading

--- src/context/provider/cart.tsx
+++ src/context/provider/cart.tsx
@@ -1,17 +1,23 @@
 'use client'
 
-import { createContext, useReducer } from 'react'
+import { createContext, useReducer, useEffect } from 'react'
 
 import cartReducer from '../reducer/cart';
 
 const CartContext = createContext({});
 
+const cartLocal = JSON.parse(localStorage.getItem('cart'))
+
 const initialState = {
-    cart: {},
+    cart: cartLocal || {},
 };
 
 const CartContextProvider = ({ children }: any) => {
     const [state, dispatch] = useReducer(cartReducer, initialState);
+
+    useEffect(() => {
+        localStorage.setItem('cart', JSON.stringify(state.cart))
+    }, [state])
 
     return (
         <CartContext.Provider value={{ state, dispatch }}>
